38103e70507cab4d8ec36393b262ec524dcac8a3
arja sivapiragasam <arjasiv@gmail.com>
2026-01-31 15:06:09 +0100

    Chair and mutant script update with splatter

--- scenes/mutant.gd
+++ scenes/mutant.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 
 @onready var originalyposition : float = position.y
@@ -11,5 +11,12 @@ func _ready() -> void:
 	timer.timeout.connect(_on_timer_timeout)
 	
 func _on_timer_timeout() -> void:
-	var tween := create_tween()
-	tween.tween_property(self, "position:y", originalyposition, 0.2)
+	var tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(self, "position:y", originalyposition, 0.7)
+	
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+		print("event click")
+		#arja.mutant_hit.emit()
+		
