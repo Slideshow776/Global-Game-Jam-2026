7dd3a77dbc395ba9fcd1b63b8c95686546dcae53
arja sivapiragasam <arjasiv@gmail.com>
2026-01-31 16:13:05 +0100

    Detecbt body part + bloodsplatter

--- scenes/mutant.gd
+++ scenes/mutant.gd
@@ -10,23 +10,15 @@ func _ready() -> void:
 	originalyposition = position.y
 	position.y = -1000 
 	
-	pass
+	var mutantColor: Color = Color(randf_range(0.1, 1.0), randf_range(0.1, 1.0), randf_range(0.1, 1.0), 1)
+	self_modulate = mutantColor
 	
 func _on_timer_timeout() -> void:
-	
 	var tween := create_tween()
 	tween.tween_property(self, "position:y", originalyposition, 0.3)
-	
-	pass # Replace with function body.
 
-#func _on_area_2d_area_entered(area: Area2D) -> void:
-	#print("area 2d entered")
-	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-	#	mutantHit.emit()
-	#pass # Replace with function body.
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-		print("event click")
-		#arja.mutant_hit.emit()
+#func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+#	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+#		print("event click")
+#		GameState.mutant_hit.emit()
 		
