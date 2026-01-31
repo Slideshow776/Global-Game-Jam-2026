09015c3bde45d7068faebd4b04bc5bd2e3523b19
Thobla <thorgalblanco@gmail.com>
2026-01-31 11:59:25 +0100

    Fix in mutant, and cursor now on gunpoint
    Also hides mouse cursor :)

--- game_state.gd
+++ game_state.gd
@@ -4,6 +4,7 @@ signal gun_clicked
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gun_clicked.connect(gun_play_sound)
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 func helloWorld():
 	print_debug("hello world")
--- scenes/mutant.gd
+++ scenes/mutant.gd
@@ -1,17 +1,18 @@
 extends Sprite2D
 
 
-var originalyposition : float 
+@onready var originalyposition : float = position.y
+@onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	originalyposition = position.y
+	# originalyposition = position.y
 	position.y = -1000 
-	
+	timer.timeout.connect(_on_timer_timeout)
 	pass
 	
 func _on_timer_timeout() -> void:
-	
+	print_debug("hello")
 	var tween := create_tween()
 	tween.tween_property(self, "position:y", originalyposition, 0.2)
 	
