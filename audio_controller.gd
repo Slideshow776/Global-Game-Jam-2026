284d8641d4d33250fb50700a185e2e975a0d99f6
Sandoras <Simenstenrod@gmail.com>
2026-01-31 14:40:52 +0100

    Added ambience audio files and audio_controller scene

---
+++ audio_controller.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var mute: bool = false
+
+func _ready():
+	
+	
+	
+	if not mute:
+		$Atmos.play()
+		await get_tree().create_timer(2.0).timeout
+		#Enviroment change
+		var AtmosPlayback: AudioStreamPlaybackInteractive = $Atmos.get_stream_playback()
+		if AtmosPlayback.is_playing():
+			AtmosPlayback.switch_to_clip(1)
+			print("Switched to clip 1")
