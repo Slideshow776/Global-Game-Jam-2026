757e865a4e19b39fd7c373e1cf8db4ca28ca1956
arja sivapiragasam <arjasiv@gmail.com>
2026-02-01 12:47:29 +0100

    Mutant 5

--- game_state.gd
+++ game_state.gd
@@ -7,13 +7,19 @@ signal mutant_died
 
 signal brain_remove
 
+signal change_health(health)
+signal end_game
+
 var mutant_chair_anchor = Vector2(289, 501)
+
+var current_health = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gun_clicked.connect(gun_play_sound)
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	brain_hit.connect(hello_world)
+	change_health.connect(update_health)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -25,3 +31,8 @@ func gun_play_sound():
 
 func hello_world():
 	print_debug("Hello world")
+	
+func update_health(health):
+	current_health += health
+	if current_health <= 0:
+		end_game.emit()
