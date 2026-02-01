b2e2b1e2fab5c39d9e13301de0bbf338d5789fa0
Juksefantomet <larsgrav@gmail.com>
2026-02-01 12:29:47 +0100

    Add exray vision

--- game_state.gd
+++ game_state.gd
@@ -7,6 +7,10 @@ signal mutant_died
 
 signal brain_remove
 
+signal toggle_exray(state: bool)
+
+var exray_enabled = false
+
 var mutant_chair_anchor = Vector2(289, 501)
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +18,10 @@ func _ready() -> void:
 	gun_clicked.connect(gun_play_sound)
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	brain_hit.connect(hello_world)
+	toggle_exray.emit(false)
+
+func flip_exray(state: bool):
+	exray_enabled = state
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
