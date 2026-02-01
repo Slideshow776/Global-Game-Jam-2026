307368bb2ec26230f9257b85dd93e73c6d1baef4
Juksefantomet <larsgrav@gmail.com>
2026-02-01 10:11:18 +0100

    Add the new mainscene script

---
+++ tempScenes/MainScene.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+@onready var mutant_array: Array[PackedScene] = [
+	preload("res://scenes/mutant.tscn")
+]
+@onready var mutantCell: Node2D = $mutantCell
+@onready var mutant: PackedScene = preload("res://scenes/mutant.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GameState.mutant_spawn.connect(create_mutant)
+	var tempMutant = mutant.instantiate()
+	mutantCell.add_child(tempMutant)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func create_mutant():
+	var mutant = mutant_array[0].instantiate()
+	mutantCell.add_child(mutant)
