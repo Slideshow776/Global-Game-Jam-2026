902be7a4bc8c57529e5c365fc340afa4a9129bd8
Juksefantomet <larsgrav@gmail.com>
2026-02-01 09:59:54 +0100

    Add brain script

---
+++ tempScenes/brain.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var brain_sprite: Sprite2D = $BrainSprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			print(name)
+			GameState.brain_hit.emit()
+			queue_free()
